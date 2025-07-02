33208b190e644710a25229c6ee95a5bd33ce9f93
liushengtao <liushengtao@addcn.com>
2025-07-02 16:57:59 +0800

    触决同步发送邮件时的协程错误

--- src/Channels/MailChannel.php
+++ src/Channels/MailChannel.php
@@ -6,6 +6,7 @@ namespace Apffth\Hyperf\Notification\Channels;
 
 use Apffth\Hyperf\Notification\Notification;
 use Hyperf\Context\ApplicationContext;
+use Hyperf\Coroutine\Parallel;
 use Hyperf\Logger\LoggerFactory;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
@@ -24,15 +25,24 @@ class MailChannel implements ChannelInterface
         $email->from(new Address(config('mail.from.address'), config('mail.from.name')))
             ->to(new Address($this->getEmail($notifiable)));
 
-        try {
+        // try {
+        if ($notification->shouldQueue($notifiable)) {
             $this->mailer->send($email);
-        } catch (Throwable $th) {
-            ApplicationContext::getContainer()
-                ->get(LoggerFactory::class)
-                ->get('notification', 'default')
-                ->error('Failed to send email: ' . $th->__toString());
-            throw $th;
+        } else {
+            $parallel = new Parallel();
+            $parallel->add(function () use ($email) {
+                return $this->mailer->send($email);
+            });
+
+            $parallel->wait();
         }
+        // } catch (Throwable $th) {
+        //     ApplicationContext::getContainer()
+        //         ->get(LoggerFactory::class)
+        //         ->get('notification', 'default')
+        //         ->error('Failed to send email: ' . $th->__toString());
+        //     throw $th;
+        // }
     }
 
     protected function getEmail($notifiable): ?string
